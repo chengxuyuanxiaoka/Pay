981efc2ead32bbf242c4dfd49124d118401aaf06
hanteng <1246901487@qq.com>
2016-12-22 17:25:52 +0800

    支付宝、微信sdk支付流程

---
+++ PaySDK/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PaySDK
+//
+//  Created by 一米阳光 on 16/12/22.
+//  Copyright © 2016年 一米阳光. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
